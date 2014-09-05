a046aa0667da3aee48f57713ec9c9bdfc6e4fb10
mackentan <jundongtan@gmail.com>
2014-09-05 15:44:16 +0800

    add

--- doc/scanner.sh
+++ doc/scanner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 /macken/gocheck/scanner > /macken/gocheck/scannerlog1 &
-/macken/gocheck/scanner > /macken/gocheck/scannerlog2 &
-/macken/gocheck/scanner > /macken/gocheck/scannerlog3 &
-/macken/gocheck/scanner > /macken/gocheck/scannerlog4 &
+#/macken/gocheck/scanner > /macken/gocheck/scannerlog2 &
+#/macken/gocheck/scanner > /macken/gocheck/scannerlog3 &
+#/macken/gocheck/scanner > /macken/gocheck/scannerlog4 &
