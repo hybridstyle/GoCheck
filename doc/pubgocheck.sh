27cfb8b59db62b1c991d4d808bcf9919599287b6
mackentan <jundongtan@gmail.com>
2014-09-05 14:51:04 +0800

    add

--- doc/pubgocheck.sh
+++ doc/pubgocheck.sh
@@ -4,10 +4,17 @@ SPATH=$(cd "$(dirname "$0")"; pwd)
 
 SVN_PATH="/macken/build/GoCheck"
 BUILD_PATH="/macken/build/buildgocheck"
+DST_PATH="/macken/gocheck/"
 
 cd $SVN_PATH
 git pull
 rsync -az --delete --exclude=".git" $SVN_PATH/* $BUILD_PATH/
 
-cp -f $BUILD_PATH/src/*.go /macken/gocheck/
+cp -f $BUILD_PATH/src/*.go $DST_PATH
 cp -f $BUILD_PATH/doc/pubgocheck.sh /macken
+
+cd $DST_PATH
+go build checker.go
+go build scanner.go
+go build server.go
+
